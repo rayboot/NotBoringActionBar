b8543301d916fab39d6f9fe69294edc493f94334
rayboot <rayboot.work@gmail.com>
2014-07-10 16:26:42 +0800

    step 1

--- App/src/main/java/com/flavienlaurent/notboringactionbar/NoBoringActionBarActivity.java
+++ App/src/main/java/com/flavienlaurent/notboringactionbar/NoBoringActionBarActivity.java
@@ -1,138 +1,129 @@
 package com.flavienlaurent.notboringactionbar;
 
-import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Context;
-import android.content.res.Resources;
-import android.content.res.TypedArray;
-import android.graphics.Color;
-import android.graphics.RectF;
 import android.os.Bundle;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.ForegroundColorSpan;
-import android.text.style.TypefaceSpan;
-import android.util.TypedValue;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import com.nineoldandroids.view.ViewHelper;
 import java.util.ArrayList;
 
-public class NoBoringActionBarActivity extends Activity {
+public class NoBoringActionBarActivity extends Activity
+{
 
     private static final String TAG = "NoBoringActionBarActivity";
-    private int mActionBarTitleColor;
-    private int mActionBarHeight;
+    @InjectView(R.id.ivUserLogo) ImageView mIvUserLogo;
+    @InjectView(R.id.ivUserSuper) ImageView mIvUserSuper;
+    @InjectView(R.id.tvUserName) TextView mTvUserName;
+    @InjectView(R.id.tvDesc) TextView mTvDesc;
+    @InjectView(R.id.tvFansAttentions) TextView mTvFansAttentions;
+    @InjectView(R.id.ivUserSex) ImageView mIvUserSex;
+    @InjectView(R.id.hsvYear) HorizontalScrollView mHsvYear;
+    @InjectView(R.id.button) Button mButton;
+    @InjectView(R.id.button2) Button mButton2;
+    @InjectView(R.id.tvActionBarTitle) TextView mTvActionBarTitle;
+    @InjectView(R.id.actionbar) RelativeLayout mActionbar;
+    @InjectView(R.id.listview) ListView mListView;
+    @InjectView(R.id.header_picture) ImageView mHeaderPicture;
+    @InjectView(R.id.rlUserLogo) RelativeLayout mRlHeaderLogoView;
+    @InjectView(R.id.header) View mHeader;
+    private View mPlaceHolderView;
     private int mHeaderHeight;
     private int mMinHeaderTranslation;
-    private ListView mListView;
-    private KenBurnsView mHeaderPicture;
-    private ImageView mHeaderLogo;
-    private View mHeader;
-    private View mPlaceHolderView;
-    private AccelerateDecelerateInterpolator mSmoothInterpolator;
 
-    private RectF mRect1 = new RectF();
-    private RectF mRect2 = new RectF();
-
-    private AlphaForegroundColorSpan mAlphaForegroundColorSpan;
-    private SpannableString mSpannableString;
-
-    private TypedValue mTypedValue = new TypedValue();
+    float distance = 0.0f;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
-        mSmoothInterpolator = new AccelerateDecelerateInterpolator();
-        mHeaderHeight = getResources().getDimensionPixelSize(R.dimen.header_height);
-        mMinHeaderTranslation = -mHeaderHeight + getActionBarHeight();
+        mHeaderHeight =
+                getResources().getDimensionPixelSize(R.dimen.header_height);
+        mMinHeaderTranslation =
+                -mHeaderHeight + getActionBarHeight() + getScrollYearHeight();
 
         setContentView(R.layout.activity_noboringactionbar);
+        ButterKnife.inject(this);
 
-        mListView = (ListView) findViewById(R.id.listview);
-        mHeader = findViewById(R.id.header);
-        mHeaderPicture = (KenBurnsView) findViewById(R.id.header_picture);
-        mHeaderPicture.setResourceIds(R.drawable.picture0, R.drawable.picture1);
-        mHeaderLogo = (ImageView) findViewById(R.id.header_logo);
-
-        mActionBarTitleColor = getResources().getColor(R.color.actionbar_title_color);
-
-        mSpannableString = new SpannableString(getString(R.string.noboringactionbar_title));
-        mAlphaForegroundColorSpan = new AlphaForegroundColorSpan(mActionBarTitleColor);
-
-        setupActionBar();
         setupListView();
     }
 
-    private void setupListView() {
+    private void setupListView()
+    {
         ArrayList<String> FAKES = new ArrayList<String>();
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 40; i++)
+        {
             FAKES.add("entry " + i);
         }
-        mPlaceHolderView = getLayoutInflater().inflate(R.layout.view_header_placeholder, mListView, false);
+        mPlaceHolderView =
+                getLayoutInflater().inflate(R.layout.view_header_placeholder,
+                        mListView, false);
         mListView.addHeaderView(mPlaceHolderView);
-        mListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, FAKES));
-        mListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+        mListView.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, FAKES));
+        mListView.setOnScrollListener(new AbsListView.OnScrollListener()
+        {
             @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
+            public void onScrollStateChanged(AbsListView view, int scrollState)
+            {
             }
 
             @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+            public void onScroll(AbsListView view, int firstVisibleItem,
+                    int visibleItemCount, int totalItemCount)
+            {
                 int scrollY = getScrollY();
-                //sticky actionbar
-                mHeader.setTranslationY(Math.max(-scrollY, mMinHeaderTranslation));
-                //header_logo --> actionbar icon
-                float ratio = clamp(mHeader.getTranslationY() / mMinHeaderTranslation, 0.0f, 1.0f);
-                interpolate(mHeaderLogo, getActionBarIconView(), mSmoothInterpolator.getInterpolation(ratio));
-                //actionbar title alpha
-                //getActionBarTitleView().setAlpha(clamp(5.0F * ratio - 4.0F, 0.0F, 1.0F));
-                //---------------------------------
-                //better way thanks to @cyrilmottier
-                setTitleAlpha(clamp(5.0F * ratio - 4.0F, 0.0F, 1.0F));
+                mHeader.setTranslationY(
+                        Math.max(-scrollY, mMinHeaderTranslation));
+                myInterpolate(mTvUserName, mTvActionBarTitle);
+                setTitleAlpha();
             }
         });
     }
 
-    private void setTitleAlpha(float alpha) {
-        mAlphaForegroundColorSpan.setAlpha(alpha);
-        mSpannableString.setSpan(mAlphaForegroundColorSpan, 0, mSpannableString.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        getActionBar().setTitle(mSpannableString);
+    private void setTitleAlpha()
+    {
+        float alpha =
+                Math.max(0.0f, (distance - Math.abs(mHeader.getTranslationY())) / distance);
+        ViewHelper.setAlpha(mRlHeaderLogoView, alpha);
+        ViewHelper.setAlpha(mIvUserSex, alpha);
+        ViewHelper.setAlpha(mTvDesc, alpha);
+        ViewHelper.setAlpha(mTvFansAttentions, alpha);
     }
 
-    public static float clamp(float value, float max, float min) {
+    public static float clamp(float value, float max, float min)
+    {
         return Math.max(Math.min(value, min), max);
     }
 
-    private void interpolate(View view1, View view2, float interpolation) {
-        getOnScreenRect(mRect1, view1);
-        getOnScreenRect(mRect2, view2);
-
-        float scaleX = 1.0F + interpolation * (mRect2.width() / mRect1.width() - 1.0F);
-        float scaleY = 1.0F + interpolation * (mRect2.height() / mRect1.height() - 1.0F);
-        float translationX = 0.5F * (interpolation * (mRect2.left + mRect2.right - mRect1.left - mRect1.right));
-        float translationY = 0.5F * (interpolation * (mRect2.top + mRect2.bottom - mRect1.top - mRect1.bottom));
-
-        view1.setTranslationX(translationX);
-        view1.setTranslationY(translationY - mHeader.getTranslationY());
-        view1.setScaleX(scaleX);
-        view1.setScaleY(scaleY);
+    private void myInterpolate(View view1, View view2)
+    {
+        if (distance < 1.0f)
+        {
+            distance = view1.getTop() - view2.getTop();
+        }
+        view1.setVisibility(
+                Math.abs(mHeader.getTranslationY()) > distance ? View.INVISIBLE
+                        : View.VISIBLE);
+        view2.setVisibility(
+                Math.abs(mHeader.getTranslationY()) > distance ? View.VISIBLE
+                        : View.INVISIBLE);
     }
 
-    private RectF getOnScreenRect(RectF rect, View view) {
-        rect.set(view.getLeft(), view.getTop(), view.getRight(), view.getBottom());
-        return rect;
-    }
-
-    public int getScrollY() {
+    public int getScrollY()
+    {
         View c = mListView.getChildAt(0);
-        if (c == null) {
+        if (c == null)
+        {
             return 0;
         }
 
@@ -140,35 +131,23 @@ public class NoBoringActionBarActivity extends Activity {
         int top = c.getTop();
 
         int headerHeight = 0;
-        if (firstVisiblePosition >= 1) {
+        if (firstVisiblePosition >= 1)
+        {
             headerHeight = mPlaceHolderView.getHeight();
         }
 
         return -top + firstVisiblePosition * c.getHeight() + headerHeight;
     }
 
-    private void setupActionBar() {
-        ActionBar actionBar = getActionBar();
-        actionBar.setIcon(R.drawable.ic_transparent);
-
-        //getActionBarTitleView().setAlpha(0f);
+    public int getActionBarHeight()
+    {
+        return getResources().getDimensionPixelSize(
+                R.dimen.mine_actionbar_height);
     }
 
-    private ImageView getActionBarIconView() {
-        return (ImageView) findViewById(android.R.id.home);
-    }
-
-    /*private TextView getActionBarTitleView() {
-        int id = Resources.getSystem().getIdentifier("action_bar_title", "id", "android");
-        return (TextView) findViewById(id);
-    }*/
-
-    public int getActionBarHeight() {
-        if (mActionBarHeight != 0) {
-            return mActionBarHeight;
-        }
-        getTheme().resolveAttribute(android.R.attr.actionBarSize, mTypedValue, true);
-        mActionBarHeight = TypedValue.complexToDimensionPixelSize(mTypedValue.data, getResources().getDisplayMetrics());
-        return mActionBarHeight;
+    public int getScrollYearHeight()
+    {
+        return getResources().getDimensionPixelSize(
+                R.dimen.mine_scroll_year_height);
     }
 }
